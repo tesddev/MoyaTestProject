f8e195a321d86df8b977420ef3a59d4160a027fc
tesddev <tesleem.amuda@gmail.com>
2022-07-30 12:06:39 +0100

    Initial Commit

---
+++ MoyaTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoyaTestProject
+//
+//  Created by GIGL iOS on 30/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
